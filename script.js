465e53cc51bf1121d3aa055b7aea4fe684c63bea
sweepas <47362063+sweepas@users.noreply.github.com>
2024-01-19 20:52:03 +0000

    final-commit

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ function setScrollVar(){
 }
 
 setScrollVar()
+
+
+const observer = new IntersectionObserver(entries =>{
+  for(let i = entries.length - 1; i >= 0; i--){
+    const entry = entries[i]
+    if(entry.isIntersecting){
+      document.querySelectorAll("[data-img]").forEach((img)=>{
+        img.classList.remove('show')
+      })
+      const img = document.querySelector(entry.target.dataset.imgToShow)
+      img?.classList.add('show')
+      break
+    }
+  }
+}
+)
+
+document.querySelectorAll("[data-img-to-show]").forEach(section=>{
+  observer.observe(section)
+})
